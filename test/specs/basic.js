6236f1d7846e594d9b25e9aafec351f51876fb83
Anil Kumar krishanshetty <anilbms75@gmail.com>
2019-12-03 17:42:52 +0100

    inital working demo example

---
+++ test/specs/basic.js
@@ -0,0 +1,28 @@
+const assert = require("assert");
+const SEARCH_RESULT_SELECTOR = '[e2e-test-selector="search-results-number"]';
+const SEARCH_RESULT_VALUE_ATTRIBUTE = "e2e-test-results-value";
+const checkSearchResultsCount = async expectedCount => {
+  const results = await $(SEARCH_RESULT_SELECTOR);
+  const resultsCount = await results.getAttribute(
+    SEARCH_RESULT_VALUE_ATTRIBUTE
+  );
+  assert.strictEqual(parseInt(resultsCount), expectedCount);
+};
+describe("Dynamic test data attribute value page", async () => {
+  beforeEach(async () => {
+    await browser.url("https://llyzf.csb.app/");
+  });
+  it("should show zero as initial results", async () => {
+    await checkSearchResultsCount(0);
+  });
+  it("should show 25 as results when user selects to show 25 results", async () => {
+    const btn25Result = await $('[e2e-test-selector="btn-25-results"]');
+    await btn25Result.click();
+    await checkSearchResultsCount(25);
+  });
+  it("should show 50 as results when user selects to show 50 results", async () => {
+    const btn25Result = await $('[e2e-test-selector="btn-50-results"]');
+    await btn25Result.click();
+    await checkSearchResultsCount(50);
+  });
+});
